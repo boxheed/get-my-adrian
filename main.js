02a5b47f9e537c7a1dcece80018948040e7a51a6
Andrew Dunn <boxheed@users.noreply.github.com>
2025-11-12 22:16:59 +0000

    feat: added theme and switched to vite

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import Reveal from './node_modules/reveal.js/dist/reveal.esm.js';
 import Markdown from './node_modules/reveal.js/plugin/markdown/markdown.esm.js';
 
 import './node_modules/reveal.js/dist/reveal.css';
-import './node_modules/reveal.js/dist/theme/simple.css';
+import './node_modules/reveal.js/dist/theme/black.css';
 
 let deck = new Reveal({
    plugins: [ Markdown ]
